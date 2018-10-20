685187bf5ec88a3d7c02981584fb6d1eac53d1b9
Nikita Ganin <nikitaganin08@gmail.com>
2018-10-20 13:21:41 +0300

    3.14 init and create meals table

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,13 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id) VALUES
+  ('2015-06-01 14:00:00', 'Обед админа', 600, 100001),
+  ('2015-06-01 21:00:00', 'Ужин админа', 300, 100001),
+  ('2015-05-30 10:00:00', 'Завтра', 500, 100000),
+  ('2015-05-30 13:00:00', 'Обед', 1000, 100000),
+  ('2015-05-30 20:00:00', 'Ужин', 500, 100000),
+  ('2015-05-31 10:00:00', 'Завтра', 500, 100000),
+  ('2015-05-31 13:00:00', 'Обед', 1000, 100000),
+  ('2015-05-31 20:00:00', 'Ужин', 510, 100000)
